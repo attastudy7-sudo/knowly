354584813a7a6b1b6d7d06e3aa5caabb653f5b4a
Alan Focus <attastudy7@gmail.com>
2026-02-21 13:12:45 +0000

    Minor landing page improvements

--- config.py
+++ config.py
@@ -144,8 +144,9 @@ class Config:
     # ============================================================================
     
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or os.environ.get('MAIL_USERNAME')
