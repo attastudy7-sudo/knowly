1e75e0108b15ee930439f8a0faa7949e2df22b08
Alan Focus <attastudy7@gmail.com>
2026-03-09 23:33:03 +0000

    merge migration heads

---
+++ migrations/versions/7a29b26a39e2_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 7a29b26a39e2
+Revises: a1b2c3d4e5f6, c4cf0f908c9d
+Create Date: 2026-03-09 23:31:07.376514
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7a29b26a39e2'
+down_revision = ('a1b2c3d4e5f6', 'c4cf0f908c9d')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
